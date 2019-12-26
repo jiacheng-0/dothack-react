098521917c9ec13c99287d7247ee97b6d9520d39
elihuansen <elihuansen@gmail.com>
2019-12-26 17:42:28 +0800

    Vanilla js example v0.1

---
+++ normal-js-blog/myjavascript.js
@@ -0,0 +1,7 @@
+/*
+ * CMD / CTRL + S saving this file should trigger a page reload
+ * if you not a **** and installed Live Server correctly. :3
+ * https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
+*/
+
+// alert("I AM JAVASCRIPT GOD");
