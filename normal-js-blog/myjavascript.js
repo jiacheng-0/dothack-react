e0274209520f3d5f61a8a6b18046d5c66b19c66b
elihuansen <elihuansen@gmail.com>
2019-12-26 17:44:07 +0800

    Added eli's solution

--- normal-js-blog/myjavascript.js
+++ normal-js-blog/myjavascript.js
@@ -5,3 +5,54 @@
 */
 
 // alert("I AM JAVASCRIPT GOD");
+
+function getUsers() {
+    return fetch('https://jsonplaceholder.typicode.com/users')
+           .then(response => response.json())
+}
+
+function getPosts() {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+           .then(response => response.json())
+}
+
+let usersDiv = document.getElementById('users');
+let postsDiv = document.getElementById('user-posts');
+let allUsers = [];
+let allPosts = [];
+
+getUsers().then(users => {
+    allUsers = users
+    for (let user of allUsers) {
+        usersDiv.innerHTML += `
+            <div class="card" onclick="handleCardClick('${user.id}')">
+                <h4>${user.username}</h4>
+                <div>
+                    <pre>${JSON.stringify(user.address, null, 2)}</pre>
+                </div>
+            </div>
+        `;
+    }
+});
+
+getPosts().then(posts => allPosts = posts);
+
+function handleCardClick(userId) {
+    
+    postsDiv.innerHTML = '';
+
+    for (let post of allPosts) {
+        if (post.userId == userId) {
+            postsDiv.innerHTML += `
+                <div class="post">
+                    <span class="heading">${post.title}</span>
+                    <div class="body">
+                        <p>
+                            ${post.body}
+                        </p>
+                    </div>
+                </div>
+            `;
+        }
+    }
+}
