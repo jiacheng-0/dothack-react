15a2aa90dfa719e294e877e236958067b68eeb7d
jiacheng-0 <michael-tjc@hotmail.com>
2019-12-27 18:19:14 +0800

    summary

--- normal-js-blog/myjavascript.js
+++ normal-js-blog/myjavascript.js
@@ -5,3 +5,31 @@
 */
 
 // alert("I AM JAVASCRIPT GOD");
+
+function getUsers() {
+    return fetch('https://jsonplaceholder.typicode.com/users');
+    // this fetch is a asynchronous function
+}
+
+getUsers()
+.then(response => {
+    return response.json();
+})
+.then(users => {
+    console.log(users);
+    let userDiv = document.getElementById('users');
+
+    for (let user of users) {
+        userDiv.innerHTML += `
+            <div class="card">
+                <h4>${user.username}</h4>
+                <pre>
+                ${JSON.stringify(user.address, null, 2)}
+                
+                </pre>
+            </div>
+        `;
+        // 2 is indentation spaces
+    }
+})
+
